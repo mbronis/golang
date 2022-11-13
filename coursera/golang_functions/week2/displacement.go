fd0318b3e59dd7b9ed41ef87ad209e0fa363e068
Michał Broniś <michal.bronis@openx.com>
2022-11-13 23:03:35 +0100

    course 2 week2 assignment

---
+++ coursera/golang_functions/week2/displacement.go
@@ -0,0 +1,70 @@
+/*
+	Let us assume the following formula for
+	displacement s as a function of time t, acceleration a, initial velocity vo,
+	and initial displacement so.
+
+	s = ½ a t2 + vot + so
+
+	Write a program which first prompts the user
+	to enter values for acceleration, initial velocity, and initial displacement.
+	Then the program should prompt the user to enter a value for time and the
+	program should compute the displacement after the entered time.
+
+	You will need to define and use a function
+	called GenDisplaceFn() which takes three float64
+	arguments, acceleration a, initial velocity vo, and initial
+	displacement so. GenDisplaceFn()
+	should return a function which computes displacement as a function of time,
+	assuming the given values acceleration, initial velocity, and initial
+	displacement. The function returned by GenDisplaceFn() should take one float64 argument t,
+	representing time, and return one
+	float64 argument which is the displacement travelled after time t.
+
+	For example, let’s say that I want to assume
+	the following values for acceleration, initial velocity, and initial
+	displacement: a = 10, vo = 2, so = 1. I can use the
+	following statement to call GenDisplaceFn() to
+	generate a function fn which will compute displacement as a function of time.
+
+	fn := GenDisplaceFn(10, 2, 1)
+
+	Then I can use the following statement to
+	print the displacement after 3 seconds.
+
+	fmt.Println(fn(3))
+
+	And I can use the following statement to print
+	the displacement after 5 seconds.
+
+	fmt.Println(fn(5))
+*/
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func GenDisplaceFn(a, vo, so float64) func(float64)float64 {
+	fn := func(t float64) float64 {
+		return 0.5 * a * math.Pow(t, 2) + vo * t + so
+	}
+	return fn
+}
+
+func GetInput(m string) float64 {
+	var v float64
+	fmt.Print(m)
+	fmt.Scan(&v)
+	return v
+}
+
+func main(){
+	a := GetInput("Input acceleration: ")
+	vo := GetInput("Input initial velocity: ")
+	so := GetInput("Input initial displacement: ")
+
+	fn := GenDisplaceFn(a, vo, so)
+	fmt.Println("displacement after 3 seconds: ", fn(3))
+	fmt.Println("displacement after 5 seconds: ", fn(5))
+}
